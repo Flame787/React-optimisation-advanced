5788f286677b5de2329e05c7db32ff2edb7c1a27
Flame787 <mbrezovic77@gmail.com>
2025-02-03 17:27:30 +0100

    added comments - memo, useMemo

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,17 +1,15 @@
-import { useState } from 'react';
+import { useState, memo } from "react";
+// memo - built-in function which can be wrapped around component functions, to prevent unnecessary
+// component functions executions
 
-import IconButton from '../UI/IconButton.jsx';
-import MinusIcon from '../UI/Icons/MinusIcon.jsx';
-import PlusIcon from '../UI/Icons/PlusIcon.jsx';
-import CounterOutput from './CounterOutput.jsx';
-import { log } from '../../log.js';
+import IconButton from "../UI/IconButton.jsx";
+import MinusIcon from "../UI/Icons/MinusIcon.jsx";
+import PlusIcon from "../UI/Icons/PlusIcon.jsx";
+import CounterOutput from "./CounterOutput.jsx";
+import { log } from "../../log.js";
 
 function isPrime(number) {
-  log(
-    'Calculating if is prime number',
-    2,
-    'other'
-  );
+  log("Calculating if is prime number", 2, "other");
   if (number <= 1) {
     return false;
   }
@@ -27,9 +25,17 @@ function isPrime(number) {
   return true;
 }
 
-export default function Counter({ initialCount }) {
-  log('<Counter /> rendered', 1);
-  const initialCountIsPrime = isPrime(initialCount);
+// adding the memo-function (wrapping around the whole component-function, which we want to export):
+// each time a component should be re-rendered (f.e. because a parent component was re-rendered,
+// memo will add the new prop value, compare the old with the new prop value, and if they are the same,
+// memo will prevent that this component-function is re-rendered).
+// This component-function will re-execute only if the prop value, or if it's internal state, has changed.
+//
+const Counter = memo(function Counter({ initialCount }) {
+  log("<Counter /> rendered", 1);
+  const initialCountIsPrime = useMemo(() => isPrime(initialCount), [initialCount]);
+  // useMemo - only to be used for complex functions/calculations
+  // useMemo also has dependencies array
 
   const [counter, setCounter] = useState(initialCount);
 
@@ -44,8 +50,8 @@ export default function Counter({ initialCount }) {
   return (
     <section className="counter">
       <p className="counter-info">
-        The initial counter value was <strong>{initialCount}</strong>. It{' '}
-        <strong>is {initialCountIsPrime ? 'a' : 'not a'}</strong> prime number.
+        The initial counter value was <strong>{initialCount}</strong>. It{" "}
+        <strong>is {initialCountIsPrime ? "a" : "not a"}</strong> prime number.
       </p>
       <p>
         <IconButton icon={MinusIcon} onClick={handleDecrement}>
@@ -58,4 +64,17 @@ export default function Counter({ initialCount }) {
       </p>
     </section>
   );
-}
+});
+
+export default Counter;
+
+// When we interact with the page and cause something to change, React compares only 2 virtual DOM states: 
+// previous Virtual DOM state with the new Virtual DOM state (not checking in real DOM). 
+// And then if there was a changed, React changes only this particular thing in real DOM, 
+// which was changed (f.e. something inside one HTML-element),
+// but it doesn't re-render the whole html-page each time when something was changed. 
+// This is important for the performance, it would not be economical to re-render the whole page each time
+// something has changed. 
+
+// If we create multiple instances of the same component, f.e. <Counter>, every component keeps her own state,
+// and those different states don't affect each other. 
